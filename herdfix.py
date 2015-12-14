3ad27b4895c1b2213db278b373fd6e83e32a2206
Michał Górny <mgorny@gentoo.org>
2015-12-14 21:03:01 +0100

    Split the replacement into separate function

--- herdfix.py
+++ herdfix.py
@@ -11,6 +11,103 @@ import lxml.etree
 import os
 import os.path
 
+
+def replace(r, herddb):
+	"""
+	Replace <herd/> with <maintainer/> elements, guessing indent.
+
+	r: XML root element
+	herddb: Herds info database
+	"""
+	herds = r.findall('herd')
+	if not herds: # yay, one file less to care about
+		return
+	maints = r.findall('maintainer')
+	if maints:
+		insertpoint = maints[-1]
+	else:
+		insertpoint = herds[-1]
+
+	# try to guess indentation
+	def all_texts(node):
+		first = True
+		for e in node:
+			if first:
+				yield node.text
+				first = False
+			yield e.tail
+
+	def all_indents(node):
+		for t in all_texts(node):
+			if t is None:
+				yield ''
+				return
+			spl = t.split('\n')
+			# go to last line without text
+			for l in spl:
+				if l.lstrip(' \t') != '':
+					break
+			# go to the last line
+			t = l[:len(l) - len(l.lstrip(' \t'))]
+			yield t
+
+	def sub_indents(node):
+		for e in node:
+			for x in all_indents(e):
+				yield x
+
+	# some random defaults
+	indent = '\t'
+	try:
+		indent = max(all_indents(r), key=len)
+	except ValueError:
+		pass
+
+	inner_indent = indent*2 if indent else '\t'
+	try:
+		inner_indent = max(sub_indents(r), key=len)
+	except ValueError:
+		pass
+
+	# start adding new herds after maintainers
+	for h in herds:
+		he = herddb[h.text.strip()]
+
+		# look for duplicate <herd/> entries
+		for m in maints:
+			if m.find('email').text.strip() == he.email:
+				m.set('type', 'herd')
+				r.remove(h)
+				break
+		else:
+			attrs = dict(h.items())
+			attrs['type'] = 'team'
+			nm = E.maintainer('\n',
+				inner_indent, E.email(he.email), '\n',
+				indent,
+				**attrs
+			)
+			nextinsert = insertpoint.getnext()
+			nm.tail = insertpoint.tail
+			if nextinsert is not None:
+				r.insert(r.index(nextinsert), nm)
+			else:
+				# avoid extra indent
+				nm.tail = '\n'
+				r.append(nm)
+			insertpoint = nm
+
+			# now we can remove it safely
+			r.remove(h)
+
+			# now fix pre-indent
+			prev = nm.getprevious()
+			if prev is not None:
+				prev.tail = '\n' + indent
+			else:
+				nm.getparent().text = '\n' + indent
+
+
 def main():
 	herdtuple = namedtuple('herdtuple', ('email', 'name'))
 	herddb = {}
@@ -33,93 +130,9 @@ def main():
 		outf = os.path.join(outtree, subpath)
 
 		xml = lxml.etree.parse(f)
-		herds = xml.getroot().findall('herd')
-		if not herds: # yay, one file less to care about
-			continue
 		r = xml.getroot()
-		maints = r.findall('maintainer')
-		if maints:
-			insertpoint = maints[-1]
-		else:
-			insertpoint = herds[-1]
 
-		# try to guess indentation
-		def all_texts(node):
-			first = True
-			for e in node:
-				if first:
-					yield node.text
-					first = False
-				yield e.tail
-		def all_indents(node):
-			for t in all_texts(node):
-				if t is None:
-					yield ''
-					return
-				spl = t.split('\n')
-				# go to last line without text
-				for l in spl:
-					if l.lstrip(' \t') != '':
-						break
-				# go to the last line
-				t = l[:len(l) - len(l.lstrip(' \t'))]
-				yield t
-		def sub_indents(node):
-			for e in node:
-				for x in all_indents(e):
-					yield x
-
-
-		# some random defaults
-		indent = '\t'
-		try:
-			indent = max(all_indents(r), key=len)
-		except ValueError:
-			pass
-
-		inner_indent = indent*2 if indent else '\t'
-		try:
-			inner_indent = max(sub_indents(r), key=len)
-		except ValueError:
-			pass
-
-		# start adding new herds after maintainers
-		for h in herds:
-			he = herddb[h.text.strip()]
-
-			# look for duplicate <herd/> entries
-			for m in maints:
-				if m.find('email').text.strip() == he.email:
-					m.set('type', 'herd')
-					r.remove(h)
-					break
-			else:
-				attrs = dict(h.items())
-				attrs['type'] = 'team'
-				nm = E.maintainer('\n',
-					inner_indent, E.email(he.email), '\n',
-					indent,
-					**attrs
-				)
-				nextinsert = insertpoint.getnext()
-				nm.tail = insertpoint.tail
-				if nextinsert is not None:
-					r.insert(r.index(nextinsert), nm)
-				else:
-					# avoid extra indent
-					nm.tail = '\n'
-					r.append(nm)
-				insertpoint = nm
-
-				# now we can remove it safely
-				r.remove(h)
-
-				# now fix pre-indent
-				prev = nm.getprevious()
-				if prev is not None:
-					prev.tail = '\n' + indent
-				else:
-					nm.getparent().text = '\n' + indent
+		replace(r, herddb)
 
 		try:
 			os.makedirs(os.path.dirname(outf))
@@ -135,6 +148,7 @@ def main():
 		# yay, add trailing newline because lxml is dumb
 		with open(outf, 'ab') as f:
 			f.write(b'\n')
+
 
 if __name__ == '__main__':
 	main()
